d34925826b7f13d4cf613e50e40451866ffbfd9d
Jamz06 <jamshaha@mail.ru>
2018-07-20 16:17:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # настройки для БД
 # Это путь к файлу с  базой данных
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://sgo_man:YW2Z5C83HFy6bcxmaGD9@localhost/sgo?charset=utf8mb4'
+# Указать пароль при развертывании
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://sgo_man:<password>@localhost/sgo?charset=utf8mb4'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # Активирует предотвращение поддельных межсайтовых запросов.
 CSRF_ENABLED = True
 # Используется для создания криптографического токена, который используется при валидации формы
-SECRET_KEY = 'mxP7U87p6htYeCgJNR9k'
+# При развертывании указать ключ
+SECRET_KEY = ''
 
 relative_dir = '/sgo'
 
